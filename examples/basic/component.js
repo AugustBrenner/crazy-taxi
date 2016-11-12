848a12e6abc71bd333c5c89b17e30f2487accffa
Aris Pikeas <pikeas@gmail.com>
2016-11-11 21:39:28 -0800

    Fix formatting

--- examples/basic/component.js
+++ examples/basic/component.js
@@ -1,12 +1,7 @@
-
-// Dependencies =====================================================
 var c = require('crazy-taxi')
 
 
-// The component =====================================================
 module.exports = {
-   
-   // This is the view. It renders hyperscript.
     view: function(vnode) {
         return c('h1', vnode.attrs.text)
     }
