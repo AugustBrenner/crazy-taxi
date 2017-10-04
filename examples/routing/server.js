4ab89cb3862361e735a5f90cd0ed6e3467fbbbbb
August Brenner <augustbrenner@gmail.com>
2017-10-03 17:33:29 -0700

    Removed credentials and invalidated them

--- examples/routing/server.js
+++ examples/routing/server.js
@@ -8,11 +8,6 @@ const app = express()
 
 c.set('production', process.env.NODE_ENV === 'production')
 
-app.use(c.router('./routes.js').s3({
-	access_key_id: 'AKIAIGZCUFYWO2CDXW2Q',
-	secret_access_key: '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',
-	bucket_url: 'https://s3-us-west-2.amazonaws.com/wheelwell-photo-assets/site-assets/',
-	cdn_url: 'https://d3c8j4mxmubrpz.cloudfront.net/site-assets/',
-}))
+app.use(c.router('./routes.js'))
 
 app.listen(process.env.PORT || 3000)
